1c189ebc4345f6919d221fe96a189aee11a640e5
George Samusevich <electrongray@gmail.com>
2019-08-27 14:30:01 +0300

    Books_Optional_Task_Java_Classes

---
+++ Books/src/BooksComparator.java
@@ -0,0 +1,10 @@
+import java.util.Comparator;
+
+public class BooksComparator implements Comparator<Book> {
+    @Override
+    public int compare(Book bookFirst, Book bookSecond){
+        return bookFirst
+                .getPublHouse()
+                .compareTo(bookSecond.getPublHouse());
+    }
+}
