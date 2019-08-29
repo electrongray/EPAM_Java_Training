6478cb11625f60996bc027073c4c79755fe47960
George Samusevich <electrongray@gmail.com>
2019-08-29 14:45:37 +0300

    Maven_Task_Helloci

---
+++ Maven_Task_Helloci/src/main/java/com/ucoz/bosch/App.java
@@ -0,0 +1,17 @@
+package com.ucoz.bosch;
+
+/**
+ * Hello CI!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello CI!" );
+    }
+    
+    public static void doNothing() {
+    	// do nothing
+    }
+}
